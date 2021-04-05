b3f0ede2901a3481eebe3d838fc1247f6ff4c8de
ruu05wwb <34163035+ruu05wwb@users.noreply.github.com>
2021-04-05 13:30:40 +0200

    Update DS_PENDING_MONTHS.sql

--- ORACLE/DS_PENDING_MONTHS.sql
+++ ORACLE/DS_PENDING_MONTHS.sql
@@ -1,40 +1,43 @@
-INSERT INTO DWSEAI01.DS_PENDING_MONTHS
-SELECT *
-FROM
-  (SELECT DISTINCT 'EU_AS400'   AS SRC,
-    CAST(ORD_DT/100 AS INTEGER) AS ORD_MO_YR_ID
-  FROM DWSATM01.DWT42016_ORD_HDR_OMS
-  WHERE COALESCE(INTGRT_CTL_AFF, CTL_AFF)                  IN ('150', '480', '040')
-  AND CAST(ORD_DT                          /100 AS INTEGER)>=202102
-  AND TO_CHAR(LAST_UPDT_DT_KEY_NO, 'YYYYMMDD')              =TO_CHAR(SYSDATE, 'YYYYMMDD')
-  UNION ALL
-  SELECT DISTINCT 'EU_HYBR_NON_EOM'                           AS SRC,
-    EXTRACT(YEAR FROM ORD_DT) *100+EXTRACT(MONTH FROM ORD_DT) AS ORD_MO_YR_ID
-  FROM DWSATM01.DWT42130_ORD_HDR_HYB T0
-  WHERE NVL(INTGRT_AFF_CD, AFF_CD)                                 IN ('160', '090', '470', '250', '430', '420', '060', '110', '210')
-  AND EXTRACT(YEAR FROM ORD_DT)     *100+EXTRACT(MONTH FROM ORD_DT)>=202102
-  AND TO_CHAR(UPDT_DT, 'YYYYMMDD')                                  =TO_CHAR(SYSDATE, 'YYYYMMDD')
-  UNION ALL
-  SELECT DISTINCT 'EU_HYBR_EOM'                                                AS SRC,
-    EXTRACT(YEAR FROM ORDER_DATE_TIME)*100+EXTRACT(MONTH FROM ORDER_DATE_TIME) AS ORD_MO_YR_ID
-  FROM DWSATM01.DWT42231_ORD_HDR_EAP
-  WHERE AFFILIATE_CODE IN ('030'
-    /*, '100', '350', '500'*/
-    )
-  AND EXTRACT(YEAR FROM ORDER_DATE_TIME)*100+EXTRACT(MONTH FROM ORDER_DATE_TIME)>=202102
-  AND TO_CHAR(UPDT_DT, 'YYYYMMDD')                                               =TO_CHAR(SYSDATE, 'YYYYMMDD')
-  UNION ALL
-  SELECT DISTINCT 'EU_ATLAS'                                                                                                                         AS SRC,
-    CAST(TO_CHAR (CAST(from_tz(CAST(ORD_DT AS TIMESTAMP),'America/Detroit') at TIME zone 'Africa/Johannesburg' AS TIMESTAMP),'YYYYMM') AS NUMBER(6)) AS ORD_MO_YR_ID
-  FROM DWSATM01.DWT42000_ORD_HDR_ATLAS
-  WHERE INTGRT_AFF_CD                                                                                                                                  = '570'
-  AND CAST(TO_CHAR (CAST(from_tz(CAST(ORD_DT AS TIMESTAMP),'America/Detroit') at TIME zone 'Africa/Johannesburg' AS TIMESTAMP),'YYYYMM') AS NUMBER(6))>=202102
-  AND TO_CHAR(UPDT_DT, 'YYYYMMDD')                                                                                                                     =TO_CHAR(SYSDATE, 'YYYYMMDD')
-  ) T0
-WHERE NOT EXISTS
-  (SELECT SRC,
-    ORD_MO_YR_ID
-  FROM DWSEAI01.DS_PENDING_MONTHS DS_PENDING_MONTHS
-  WHERE T0.SRC       =DS_PENDING_MONTHS.SRC
-  AND T0.ORD_MO_YR_ID=DS_PENDING_MONTHS.ORD_MO_YR_ID
-  )
+BEGIN
+  INSERT INTO DWSEAI01.DS_PENDING_MONTHS
+  SELECT *
+  FROM
+    (SELECT DISTINCT 'EU_AS400'   AS SRC,
+      CAST(ORD_DT/100 AS INTEGER) AS ORD_MO_YR_ID
+    FROM DWSATM01.DWT42016_ORD_HDR_OMS
+    WHERE COALESCE(INTGRT_CTL_AFF, CTL_AFF)                  IN ('150', '480', '040')
+    AND CAST(ORD_DT                          /100 AS INTEGER)>=202102
+    AND TO_CHAR(LAST_UPDT_DT_KEY_NO, 'YYYYMMDD')              =TO_CHAR(SYSDATE, 'YYYYMMDD')
+    UNION ALL
+    SELECT DISTINCT 'EU_HYBR_NON_EOM'                           AS SRC,
+      EXTRACT(YEAR FROM ORD_DT) *100+EXTRACT(MONTH FROM ORD_DT) AS ORD_MO_YR_ID
+    FROM DWSATM01.DWT42130_ORD_HDR_HYB T0
+    WHERE NVL(INTGRT_AFF_CD, AFF_CD)                                 IN ('160', '090', '470', '250', '430', '420', '060', '110', '210')
+    AND EXTRACT(YEAR FROM ORD_DT)     *100+EXTRACT(MONTH FROM ORD_DT)>=202102
+    AND TO_CHAR(UPDT_DT, 'YYYYMMDD')                                  =TO_CHAR(SYSDATE, 'YYYYMMDD')
+    UNION ALL
+    SELECT DISTINCT 'EU_HYBR_EOM'                                                AS SRC,
+      EXTRACT(YEAR FROM ORDER_DATE_TIME)*100+EXTRACT(MONTH FROM ORDER_DATE_TIME) AS ORD_MO_YR_ID
+    FROM DWSATM01.DWT42231_ORD_HDR_EAP
+    WHERE AFFILIATE_CODE IN ('030'
+      /*, '100', '350', '500'*/
+      )
+    AND EXTRACT(YEAR FROM ORDER_DATE_TIME)*100+EXTRACT(MONTH FROM ORDER_DATE_TIME)>=202102
+    AND TO_CHAR(UPDT_DT, 'YYYYMMDD')                                               =TO_CHAR(SYSDATE, 'YYYYMMDD')
+    UNION ALL
+    SELECT DISTINCT 'EU_ATLAS'                                                                                                                         AS SRC,
+      CAST(TO_CHAR (CAST(from_tz(CAST(ORD_DT AS TIMESTAMP),'America/Detroit') at TIME zone 'Africa/Johannesburg' AS TIMESTAMP),'YYYYMM') AS NUMBER(6)) AS ORD_MO_YR_ID
+    FROM DWSATM01.DWT42000_ORD_HDR_ATLAS
+    WHERE INTGRT_AFF_CD                                                                                                                                  = '570'
+    AND CAST(TO_CHAR (CAST(from_tz(CAST(ORD_DT AS TIMESTAMP),'America/Detroit') at TIME zone 'Africa/Johannesburg' AS TIMESTAMP),'YYYYMM') AS NUMBER(6))>=202102
+    AND TO_CHAR(UPDT_DT, 'YYYYMMDD')                                                                                                                     =TO_CHAR(SYSDATE, 'YYYYMMDD')
+    ) T0
+  WHERE NOT EXISTS
+    (SELECT SRC,
+      ORD_MO_YR_ID
+    FROM DWSEAI01.DS_PENDING_MONTHS DS_PENDING_MONTHS
+    WHERE T0.SRC       =DS_PENDING_MONTHS.SRC
+    AND T0.ORD_MO_YR_ID=DS_PENDING_MONTHS.ORD_MO_YR_ID
+    );
+  COMMIT;
+END;
